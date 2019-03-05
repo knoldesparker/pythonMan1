659ed091367eec48556fb347fb0362496a986f41
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2019-03-05 14:12:18 +0100

    clone is now a thing

--- main.py
+++ main.py
@@ -1,20 +1,52 @@
 from urllib.request import urlopen
 from urllib.error import HTTPError
 import subprocess
+import os
 
+#Getting API file from Github
 """
 try:
     res = urlopen('https://api.github.com/orgs/python-elective-1-spring-2019/repos?per_page=100')
     resault = res.read().decode('utf-8')
-    
 except HTTPError as hTTPeR:
     print(hTTPeR)
 """
 
+urlList = []
+#Method for searching the file
+def search(filename, filter):
+    try:
+        with open(filename) as f:
+            for line in f:
+                if filter in line:
+                    line = line[:]
+                    yield line[18:-3].strip()
+    except FileNotFoundError as fNFE:
+        print(fNFE)
+    except Exception as err:
+        print(err)
+
 try:
-    file = open('localAPI.txt','r+')
-    output = file.read()
-    print(output)
-except FileNotFoundError as fNFeR:
-    print(fNFeR)
+    for result in search('localAPI.txt', 'clone_url'):
+        urlList.append(result)
+except Exception:
+    print(Exception)
+
+try:
+    if "repos" not in os.listdir('.'):
+        os.mkdir("repos")
+        os.chdir("repos")
+    else:
+        os.chdir("repos")
+except Exception as err:
+    print(err)
+
+for element in urlList:
+    subprocess.run(['git','clone',element])
+
+
+
+
+
+
 
