12263b6c04d8c97ecbb2b0a7c7ac0cf5d37ca0d5
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2019-03-06 00:55:04 +0100

    stuck on reading filter

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from urllib.request import urlopen
 from urllib.error import HTTPError
 import subprocess
 import os
+import sys
+from glob import iglob
+import glob
+import re
 
 #Getting API file from Github
 """
@@ -13,6 +17,7 @@ except HTTPError as hTTPeR:
 """
 
 urlList = []
+readMeList = []
 #Method for searching the file
 def search(filename, filter):
     try:
@@ -26,6 +31,7 @@ def search(filename, filter):
     except Exception as err:
         print(err)
 
+#Appends the elements to the urllist
 try:
     for result in search('localAPI.txt', 'clone_url'):
         urlList.append(result)
@@ -36,13 +42,47 @@ try:
     if "repos" not in os.listdir('.'):
         os.mkdir("repos")
         os.chdir("repos")
+        print("Creating dir")
     else:
         os.chdir("repos")
+        print("dir exist, swapping dir")
 except Exception as err:
     print(err)
+except FileNotFoundError as fNFE:
+    print(fNFE)
 
-for element in urlList:
-    subprocess.run(['git','clone',element])
+"""
+for elm in urlList:
+    if not os.path.exists(elm[49:-4]):
+            print("cloning from git")
+            subprocess.run(['git','clone', elm])
+    else:
+            print("pulling from git")
+            subprocess.run(['git', 'pull'])
+"""
+
+"""
+for filename in glob.iglob('**/README.md', recursive=True):
+    print(filename)
+"""
+rr = []
+file_list = [f for f in iglob('**/README.md', recursive=True) if os.path.isfile(f)]
+for f in file_list:
+    with open(f,'rt') as fl:
+        lines = fl.read()
+        rr.append(lines)
+    print(rr)
+
+"""
+for fp in file_list:
+        with open(fp, 'r') as f:
+            # Read the first line
+            rReading = f.read()
+            # Append the first line into the list
+            rr.append(first_line)
+        print(rr)
+"""
+
 
 
 
